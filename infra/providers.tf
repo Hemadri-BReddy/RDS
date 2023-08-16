f6e8ef293be64f28ab0a4b0c23c3e8462702bd49
Sudheer Reddy <119945374+SudheerReddyCTEPL@users.noreply.github.com>
2023-08-16 23:41:17 +0530

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -27,7 +27,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
   #profile = "AWSAdministratorAccess-194510975891"
   #access_key = var.aws_access_key
   #secret_key = var.aws_secret_key
