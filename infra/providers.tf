414e34e102c794c7c7c4a9d22a61753783c7a60c
Hemadri-BReddy <112164206+Hemadri-BReddy@users.noreply.github.com>
2023-08-21 12:42:18 +0530

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -27,7 +27,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "ca-central-1"
+  region = "eu-west-1"
   #profile = "AWSAdministratorAccess-194510975891"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
